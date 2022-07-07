56d21e215c5ebf144ceef96e68364e3432063053
thanhhai2610 <thanhai707726@gmail.com>
2022-07-07 20:53:31 +0700

    nộp bài tập ngày 7/72022

---
+++ ss6_tong_qun_javascript/thuc_hanh/hiern_thi_dau_ra.js
@@ -0,0 +1,11 @@
+// Hiển thị kết quả sử dụng thẻ HTML:
+function show() {
+console.log(`Xin chào console`)
+}
+// Hiển thị kết quả sử dụng hàm document.write():
+function show1() {
+    document.write(`Xin chào 1`)
+}
+function show2() {
+    alert(`Xin chào 1`)
+}
