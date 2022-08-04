f7d3bb4bc232bb9ba150c613f68a532346cfbed3
thanhhai2610 <thanhai707726@gmail.com>
2022-08-04 21:07:27 +0700

    nộp bài tập ngày 04/08/2022

--- Module_02/src/ss4_lop_va_doi_tuong_trong_java/xay_dung_lop_fan/Fan.java
+++ Module_02/src/ss4_lop_va_doi_tuong_trong_java/xay_dung_lop_fan/Fan.java
@@ -1,4 +1,60 @@
 package ss4_lop_va_doi_tuong_trong_java.xay_dung_lop_fan;
 
 public class Fan {
+    //    final int SLOW = 1;
+//    final int MEDIUM = 2;
+//    final int FAST = 3;
+    private int speed;
+    private boolean on;
+    private double radius;
+    private String color;
+
+
+    public Fan(int speed, boolean on, double radius, String color) {
+        this.speed = speed;
+        this.on = on;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public boolean isOn() {
+        return on;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public void setOn(boolean on) {
+        this.on = on;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        if (on) {
+            return this.speed + this.color + this.radius + "fan is on";
+        } else {
+            return this.speed + this.color + this.radius + "fan is off";
+        }
+    }
 }
--- Module_02/src/ss4_lop_va_doi_tuong_trong_java/xay_dung_lop_fan/Main.java
+++ Module_02/src/ss4_lop_va_doi_tuong_trong_java/xay_dung_lop_fan/Main.java
@@ -1,4 +1,10 @@
 package ss4_lop_va_doi_tuong_trong_java.xay_dung_lop_fan;
 
 public class Main {
+    public static void main(String[] args) {
+        Fan fan1=new Fan(2,true,26,"blue");
+        Fan fan2=new Fan(3,false,26,"blue");
+        System.out.println(fan1.toString());
+        System.out.println(fan2.toString());
+    }
 }
